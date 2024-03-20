3906d5de6e24f027fc1ac25636ee60bdfbcbb810
Armands Leimanis <armands@gmail.com>
2024-03-20 02:59:06 +0200

    Added ads. Sorry :(

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
 
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "index.html", component: AppComponent },
+  { path: "", redirectTo: "index.html", pathMatch: "full" },
+  { path: "**", redirectTo: "index.html", pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
